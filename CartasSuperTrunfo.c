addfeac3b838af5353d36819aac074c268188e2c
Ramon Nascimento dos santos <ramon.dev.rs@gmail.com>
2025-08-07 02:00:51 +0000

    Cadastro das duas cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,78 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+    char estado[3], estado2[3];// Comecei o Jogo declarando algumas Variaveis.
+    char codigo[10], codigo2[10];
+    char cidade[50], cidade2[50];
+    int populacao, populacao2, pontosT, pontosT2;
+    float area, area2, PIB, PIB2;
     
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    printf("------ Jogo Super Trunfo Países-------\n\n");
+    printf("--------- Cadastro de Cartas -----------\n");
+    printf("-------------- CARTA 1 -----------\n\n"); // Usei printf e scanf para mostra e armazena as informações do usuário CARTA 1.
+
+    printf("Digite uma letra para representar um Estado: \n"); 
+    scanf("%s", estado);
+
+    printf("Digite o código do Estado: \n");
+    scanf("%s", codigo);
+
+    printf("Digite o nome da Cidade: \n");
+    scanf("%s", cidade);
+
+    printf("Digite o numero de habitantes \n");
+    scanf("%d", &populacao);
+
+    printf("Digite a Area da cidade em m²: \n");
+    scanf("%f", &area);
+
+    printf("Digite o PIB (Produto interno Bruto) da cidade \n");
+    scanf("%f", &PIB);
+
+    printf("Digite a quantidade de Pontos turisticos: \n");
+    scanf("%d", &pontosT);
+
+    printf("-------------- CARTA 2 -----------\n\n"); // Usei printf e scanf para mostra e armazena as informações do usuário CARTA 2.
+    printf("Digite uma letra para representar um Estado: \n");
+    scanf("%s", estado2);
+    
+    printf("Digite o codigo do Estado \n");
+    scanf("%s", codigo2);
+
+    printf("Digite o nome da Cidade \n");
+    scanf("%s", cidade2);
+
+    printf("Digite o numero de habitantes \n");
+    scanf("%d", &populacao2);
+
+    printf("Digite a Área da cidade em m²: \n");
+    scanf("%f", &area2);
+
+    printf("Digite o PIB (Produto interno Bruto) da cidade \n");
+    scanf("%f", &PIB2);
+
+    printf("Digite a quantidade de Pontos turisticos: \n");
+    scanf("%d", &pontosT2);
+
+    printf("CARTA 1:\n");// Usei o comado printf para exibir os valores armazenado nas variaveis.
+    printf("Estado: %s\n", estado);
+    printf("Código: %s\n", codigo);
+    printf("Nome da Cidade: %s\n", cidade);
+    printf("População: %d\n", populacao);
+    printf("Área m²: %f\n", area);
+    printf("PIB: %f\n", PIB);
+    printf("Número de Pontos Turisticos: %d\n\n", pontosT);
+
+    printf("CARTA 2:\n");// usei o comado printf para exibir os valores armazenado nas variaveis.
+    printf("Estado: %s\n", estado2);
+    printf("Código: %s\n", codigo2);
+    printf("Nome da Cidade: %s\n", cidade2);
+    printf("População: %d\n", populacao2);
+    printf("Área m²: %f\n", area2);
+    printf("PIB: %f\n", PIB2);
+    printf("Número de Pontos Turisticos: %d\n\n", pontosT2);
 
     return 0;
 }
