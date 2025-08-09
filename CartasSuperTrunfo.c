5f908fb9c6f7a13d0d1b7254c74857b75e0010da
Ramon Nascimento dos santos <ramon.dev.rs@gmail.com>
2025-08-09 00:48:39 +0000

    Corrigir alguns pontos de Atenção.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,18 +3,20 @@
 
 int main() {
     
-    char estado[3], estado2[3];// Comecei o Jogo declarando algumas Variaveis.
+    // Comecei o Jogo declarando algumas Variaveis.
+    char estado, estado2;
     char codigo[10], codigo2[10];
     char cidade[50], cidade2[50];
-    int populacao, populacao2, pontosT, pontosT2;
+    unsigned long int populacao, populacao2; // unsigned log int serve para grandes populações.
+    int pontosT, pontosT2;
     float area, area2, PIB, PIB2;
     
     printf("------ Jogo Super Trunfo Países-------\n\n");
     printf("--------- Cadastro de Cartas -----------\n");
-    printf("-------------- CARTA 1 -----------\n\n"); // Usei printf e scanf para mostra e armazena as informações do usuário CARTA 1.
-
+    printf("-------------- CARTA 1 -----------\n\n"); 
+    // Usei printf e scanf para mostra e armazena as informações do usuário CARTA 1.
     printf("Digite uma letra para representar um Estado: \n"); 
-    scanf("%s", estado);
+    scanf(" %c", &estado);
 
     printf("Digite o código do Estado: \n");
     scanf("%s", codigo);
@@ -34,9 +36,11 @@ int main() {
     printf("Digite a quantidade de Pontos turisticos: \n");
     scanf("%d", &pontosT);
 
-    printf("-------------- CARTA 2 -----------\n\n"); // Usei printf e scanf para mostra e armazena as informações do usuário CARTA 2.
+    printf("-------------- CARTA 2 -----------\n\n");
+
+    // Usei printf e scanf para mostra e armazena as informações do usuário CARTA 2.
     printf("Digite uma letra para representar um Estado: \n");
-    scanf("%s", estado2);
+    scanf(" %c", &estado2);
     
     printf("Digite o codigo do Estado \n");
     scanf("%s", codigo2);
@@ -56,22 +60,24 @@ int main() {
     printf("Digite a quantidade de Pontos turisticos: \n");
     scanf("%d", &pontosT2);
 
-    printf("CARTA 1:\n");// Usei o comado printf para exibir os valores armazenado nas variaveis.
-    printf("Estado: %s\n", estado);
+    // Usei o comado printf para exibir os valores armazenado nas variaveis.
+    printf("CARTA 1:\n");
+    printf("Estado: %c\n", estado);
     printf("Código: %s\n", codigo);
     printf("Nome da Cidade: %s\n", cidade);
     printf("População: %d\n", populacao);
-    printf("Área m²: %f\n", area);
-    printf("PIB: %f\n", PIB);
+    printf("Área m²: %.2f\n", area);
+    printf("PIB: %.2f\n", PIB);
     printf("Número de Pontos Turisticos: %d\n\n", pontosT);
 
-    printf("CARTA 2:\n");// usei o comado printf para exibir os valores armazenado nas variaveis.
-    printf("Estado: %s\n", estado2);
+    // usei o comado printf para exibir os valores armazenado nas variaveis.
+    printf("CARTA 2:\n");
+    printf("Estado: %c\n", estado2);
     printf("Código: %s\n", codigo2);
     printf("Nome da Cidade: %s\n", cidade2);
     printf("População: %d\n", populacao2);
-    printf("Área m²: %f\n", area2);
-    printf("PIB: %f\n", PIB2);
+    printf("Área m²: %.2f\n", area2);
+    printf("PIB: %.2f\n", PIB2);
     printf("Número de Pontos Turisticos: %d\n\n", pontosT2);
 
     return 0;
